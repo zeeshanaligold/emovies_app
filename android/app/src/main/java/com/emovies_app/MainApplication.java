7722f769b9eabb3c40668c7b91253170b9f8d77c
Zeeshan Ali <zeeshanaligold@gmail.com>
2019-05-07 03:57:46 +0500

    Integrate FBSDK & few login page ui fixes

--- android/app/src/main/java/com/emovies_app/MainApplication.java
+++ android/app/src/main/java/com/emovies_app/MainApplication.java
@@ -2,7 +2,13 @@ package com.emovies_app;
 
 import android.app.Application;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
+
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -20,6 +26,12 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -30,6 +42,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager),
             new SplashScreenReactPackage(),
             new ReactVideoPackage(),
             new VectorIconsPackage(),
